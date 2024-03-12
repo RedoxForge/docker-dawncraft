0bd0b48f31bc502bf331134146caf496189ea5ec
Jason McLeod <jason@mcleods.me>
2024-03-12 08:37:12 -0400

    Update to version 2.0.1

--- launch.sh
+++ launch.sh
@@ -12,9 +12,9 @@ else
 	exit 9
 fi
 
-if ! [[ -f 'DawnCraft%202.0%20BETA%20Serverpack.zip' ]]; then
+if ! [[ -f 'DawnCraft%202.0.1%20Beta%20Serverpack.zip' ]]; then
 	rm -fr config kubejs libraries mods *.zip forge*.jar
-	curl -Lo 'DawnCraft%202.0%20BETA%20Serverpack.zip' 'https://edge.forgecdn.net/files/5172/825/DawnCraft%202.0%20BETA%20Serverpack.zip' && unzip -u -o 'DawnCraft%202.0%20BETA%20Serverpack.zip' -d /data
+	curl -Lo 'DawnCraft%202.0.1%20Beta%20Serverpack.zip' 'https://edge.forgecdn.net/files/5175/866/DawnCraft%202.0.1%20Beta%20Serverpack.zip' && unzip -u -o 'DawnCraft%202.0.1%20Beta%20Serverpack.zip' -d /data
   curl -Lo forge-${FORGE_VERSION}-installer.jar 'https://maven.minecraftforge.net/net/minecraftforge/forge/'${FORGE_VERSION}'/forge-'${FORGE_VERSION}'-installer.jar'
 	java -jar $(ls forge-*-installer.jar) --installServer && rm -f forge-*-installer.jar
 fi
